32c74c174f60629ef0ddecb9091d76194f6a4f24
Arpita <1234arpita@users.noreply.github.com>
2026-06-30 12:21:37 +0530

    updated class name

--- src/corejava/stream/basic/Mostasked.java
+++ src/corejava/stream/basic/Mostasked.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-public class SortEmployee {
+public class Mostasked {
     static void main() {
         List<Employee> employees = EmployeeData.getEmployees();
       //  1. sort by dept
